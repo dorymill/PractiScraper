0d1f147e540841ceb57ed72262ede59ac2c14168
dorymill <dorymill@outlook.com>
2025-02-13 06:51:10 -0500

    Added addtional delay for level selections to take effect.

--- src/main/java/pscraper/PractiScoreScraper.java
+++ src/main/java/pscraper/PractiScoreScraper.java
@@ -100,9 +100,13 @@ public class PractiScoreScraper implements Runnable {
 
         page.locator("#divisionLevel").selectOption(Integer.toString(division));
 
+        delay(5);
+
         for(stageNum = 1; stageNum < numStages + 1; stageNum++) {
 
             page.locator("#resultLevel").selectOption(Integer.toString(stageNum));
+
+            delay(5);
             
             emitState("Searching for results table. . .");
             /* Wait up to 2 minutes to find the table. */
